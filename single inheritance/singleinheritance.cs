bc1bbe9c128e261c3f75a0b612fbf36a521878f2
Haritha R <harithalaks1998@gmail.com>
2024-06-25 19:51:58 +0530

    Add project files.

---
+++ single inheritance/singleinheritance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_inheritance
+{
+
+    public class Animal
+
+    {
+        public void eat() { Console.WriteLine("Eating..."); }
+
+    }
+
+    public class Dog : Animal
+
+    {
+        public void bark() { Console.WriteLine("Barking..."); }
+
+    }
+
+    public class singleinheritance
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+
+            d1.eat();
+
+            d1.bark();
+            Console.ReadKey();
+        }
+    }
+}
